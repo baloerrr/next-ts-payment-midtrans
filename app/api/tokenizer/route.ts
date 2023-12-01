a2d269db7aee2fed7af4889ae64dd74cc783410d
baloerrr <99787895+baloerrr@users.noreply.github.com>
2023-12-02 02:04:16 +0700

    fix(midtrans token) : Add return on funtion POST

--- app/api/tokenizer/route.ts
+++ app/api/tokenizer/route.ts
@@ -1,27 +1,33 @@
-import Midtrans from 'midtrans-client'
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server';
+import Midtrans, { Snap } from 'midtrans-client';
 
-let snap = new Midtrans.Snap({
-    isProduction: false,
-    serverKey: process.env.SECRET,
-    clientKey: process.env.NEXT_PUBLIC_CLIENT
-})
+const snap = new Snap({
+  isProduction: false,
+  serverKey: process.env.SECRET || '',
+  clientKey: process.env.NEXT_PUBLIC_CLIENT || '',
+});
 
-export async function POST(request : any) {
-    const {id, productName, price, quantity} = await request.json()
+export async function POST(request: NextRequest) {
+  try {
+    const { id, productName, price, quantity } = await request.json();
 
-    let parameter = {
-        item_details: {
-            name: productName,
-            price: price,
-            quantity: quantity
-        },
-        transaction_details: {
-            order_id: id,
-            gross_amount: price * quantity
-        }
-    }
+    const parameter: Midtrans.TransactionRequestBody = {
+      item_details: {
+        name: productName,
+        price: price,
+        quantity: quantity,
+      },
+      transaction_details: {
+        order_id: id,
+        gross_amount: price * quantity,
+      },
+    };
 
-    const token = await snap.createTransactionToken(parameter)
-    NextResponse.json({token})
+    const token = await snap.createTransactionToken(parameter);
+
+    return NextResponse.json({ token });
+  } catch (error) {
+    console.error('Error processing the request:', error);
+    return NextResponse.error();
+  }
 }
