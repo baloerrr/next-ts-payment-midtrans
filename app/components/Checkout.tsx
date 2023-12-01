cbf690289e7ecf75d0694a5145964d5dd8cfd2b5
baloerrr <99787895+baloerrr@users.noreply.github.com>
2023-12-02 04:02:33 +0700

    feat(payment link) : Add generate payment link

--- app/components/Checkout.tsx
+++ app/components/Checkout.tsx
@@ -1,60 +1,95 @@
-import React, { useState, ChangeEvent } from "react";
-import { product } from "../libs/product";
-import { NextResponse } from "next/server";
+import React, { useState, ChangeEvent } from 'react'
+import { product } from '../libs/product'
+import { NextResponse } from 'next/server'
+import Link from 'next/link'
 
 const Checkout: React.FC = () => {
-  const [quantity, setQuantity] = useState<number>(1);
+  const [quantity, setQuantity] = useState<number>(1)
+  const [paymentLink, setPaymentLink] = useState<string>("")
 
   const decreaseQuantity = () => {
-    setQuantity((prevQuantity) => (prevQuantity > 1 ? prevQuantity - 1 : 1));
-  };
+    setQuantity((prevQuantity) => (prevQuantity > 1 ? prevQuantity - 1 : 1))
+  }
 
   const increaseQuantity = () => {
-    setQuantity((prevQuantity) => prevQuantity + 1);
-  };
+    setQuantity((prevQuantity) => prevQuantity + 1)
+  }
 
   const checkout = async () => {
     const data = {
       id: product.id,
       productName: product.name,
       price: product.price,
-      quantity: quantity
+      quantity: quantity,
     }
-  
+
     try {
-      const response = await fetch("/api/tokenizer", {
-        method: "POST",
-        body: JSON.stringify(data)
-      });
-  
+      const response = await fetch('/api/tokenizer', {
+        method: 'POST',
+        body: JSON.stringify(data),
+      })
+
       if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
+        throw new Error(`HTTP error! Status: ${response.status}`)
       }
-  
-      const responseData = await response.json();
-  
+
+      const responseData = await response.json()
+
       if (!responseData || !responseData.token) {
-        throw new Error("Invalid response data");
+        throw new Error('Invalid response data')
       }
-  
-      console.log(responseData);
-      (window as any).snap.pay(responseData.token);
-    } catch (error: any) {
-      console.error("Error during checkout:", error.message);
+
+      ;(window as any).snap.pay(responseData.token)
+    } catch (error) {
+      console.error('Error during checkout:', error.message)
       // Handle the error as needed
     }
   }
 
-  const generatePaymentLink = () => {
-    
-  };
+  const generatePaymentLink = async () => {
+
+    const secret: any = process.env.NEXT_PUBLIC_SECRET
+    const encodedSecret = Buffer.from(secret).toString('base64')
+    const basicAuth = `Basic ${encodedSecret}`
+
+    let data = {
+      item_details: [
+        {
+          id: product.id,
+          name: product.name,
+          price: product.price,
+          quantity: quantity,
+        }
+      ],
+      transaction_details: {
+        order_id: product.id,
+        gross_amount: product.price * quantity
+      }
+    }
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API}/v1/payment-links`,
+      {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': basicAuth,
+        },
+        body: JSON.stringify(data)
+      },
+    )
+
+    const paymentLink = await response.json()
+    setPaymentLink(paymentLink.payment_url)
+  }
 
   const handleQuantityChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const newQuantity = parseInt(e.target.value, 10);
+    const newQuantity = parseInt(e.target.value, 10)
     if (!isNaN(newQuantity) && newQuantity >= 1) {
-      setQuantity(newQuantity);
+      setQuantity(newQuantity)
     }
-  };
+  }
 
   return (
     <>
@@ -95,8 +130,10 @@ const Checkout: React.FC = () => {
       >
         Create Payment Link
       </button>
-    </>
-  );
-};
 
-export default Checkout;
+      <Link className='text-blue-600 underline' href={paymentLink} target='_blank'>{paymentLink}</Link>
+    </>
+  )
+}
+
+export default Checkout
