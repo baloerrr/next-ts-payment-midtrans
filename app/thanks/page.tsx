28b41f0fa7618a4f65d57382749b502ac17e414d
baloerrr <99787895+baloerrr@users.noreply.github.com>
2023-12-03 23:27:35 +0700

    fix(page) : Fix Thanks page

--- app/thanks/page.tsx
+++ app/thanks/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const page = () => {
     return (
         <div className='flex flex-col justify-center items-center min-h-screen gap-4'>
-            <h3 className='text-center'>Terimakasih telah melakukan pembayaran</h3>
+            <h3 className='text-center'>Terimakasih telah melakukan pembayaran Guys</h3>
             <Link href="/" className="text-center">Kembali</Link>
         </div>
     )
